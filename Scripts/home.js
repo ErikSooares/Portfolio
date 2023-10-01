b26504742a1508b4e5b540b19a60cae633315ae2
Erik <eriksoares1678@gmail.com>
2023-10-01 13:00:35 -0300

    update responsive

--- Scripts/home.js
+++ Scripts/home.js
@@ -6,36 +6,36 @@ var imgEmail = document.getElementById("imgEmail");
 
 //Alterando as imagens do link Telefone
 imgTel.addEventListener('mouseover', function(){
-    imgTel.src = '/Images/Telefone/telefoneHover.png';
+    imgTel.src = 'Images/Telefone/telefoneHover.png';
 });
 
 imgTel.addEventListener('mouseout', function(){
-    imgTel.src = '/Images/Telefone/telefone.png'; // Restaura a imagem original
+    imgTel.src = 'Images/Telefone/telefone.png'; // Restaura a imagem original
 });
 
 //Alterando as imagens do link GitHub
 imgGit.addEventListener('mouseover', function(){
-    imgGit.src = '/Images/GitHub/githubHover.png';
+    imgGit.src = 'Images/GitHub/githubHover.png';
 });
 
 imgGit.addEventListener('mouseout', function(){
-    imgGit.src = '/Images/GitHub/github.png'; // Restaura a imagem original
+    imgGit.src = 'Images/GitHub/github.png'; // Restaura a imagem original
 });
 
 //Alterando as imagens do link LinkedIn
 imgLink.addEventListener('mouseover', function(){
-    imgLink.src = '/Images/LinkedIn/linkedinHover.png';
+    imgLink.src = 'Images/LinkedIn/linkedinHover.png';
 });
 
 imgLink.addEventListener('mouseout', function(){
-    imgLink.src = '/Images/LinkedIn/linkedin.png'; // Restaura a imagem original
+    imgLink.src = 'Images/LinkedIn/linkedin.png'; // Restaura a imagem original
 });
 
 //Alterando as imagens do link Email
 imgEmail.addEventListener('mouseover', function(){
-    imgEmail.src = '/Images/Email/emailHover.png';
+    imgEmail.src = 'Images/Email/emailHover.png';
 });
 
 imgEmail.addEventListener('mouseout', function(){
-    imgEmail.src = '/Images/Email/email.png'; // Restaura a imagem original
+    imgEmail.src = 'Images/Email/email.png'; // Restaura a imagem original
 });
