86f311832203fb523df22445d5b6ecef46ab872f
Erik <eriksoares1678@gmail.com>
2023-10-04 10:12:22 -0300

    update menu

--- Scripts/menuOpen.js
+++ Scripts/menuOpen.js
@@ -1,12 +1,15 @@
 var menuImg = document.getElementById("menuImg");
 var menuOpen = document.getElementById("menu-open");
 var menuImgFechado = document.getElementById("menuImgFechado");
+var body = document.body;
 
-menuImg.addEventListener('click', function(){
+menuImg.addEventListener('click', function(e){
     menuOpen.style.cssText = (
         "transition: 0.2s;" +
         "display: block;"
     );
+    body.classList.add("menu-open"); // Adiciona a classe menu-open ao body
+    e.stopPropagation();
 })
 
 menuImgFechado.addEventListener('click', function(){
@@ -14,4 +17,15 @@ menuImgFechado.addEventListener('click', function(){
         "transition: 0.2s;" +
         "display: none;"   
     );
+    body.classList.remove("menu-open"); // Remove a classe menu-open do body
 })
+
+document.addEventListener('click', function(e) {
+    if (!menuOpen.contains(e.target) && e.target !== menuImg) {
+        menuOpen.style.cssText = (
+            "transition: 0.2s;" +
+            "display: none;"
+        );
+        body.classList.remove("menu-open"); // Remove a classe menu-open do body
+    }
+});
